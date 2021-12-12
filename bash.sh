488a5a11bd9d58ce82b6c1cf413458f9751cb631
ZPdesu <peihao.zhu@kaust.edu.sa>
2021-12-12 08:17:24 +0300

    update

--- bash.sh
+++ bash.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+python main.py --ckpt pretrained_models/ffhq.pt --PCA_path pretrained_models/PCA_ffhq.npz --size 1024
+
 #python main.py --ckpt pretrained_models/metfaces.pt --PCA_path pretrained_models/PCA_metfaces.npz --size 1024
 
 #python main.py --ckpt pretrained_models/afhqcat.pt --PCA_path pretrained_models/PCA_afhqcat.npz --size 512
-#
+
 #python main.py --ckpt pretrained_models/afhqwild.pt --PCA_path pretrained_models/PCA_afhqwild.npz --size 512
 
-python main.py --ckpt pretrained_models/afhqdog.pt --PCA_path pretrained_models/PCA_afhqdog.npz --size 512
+#python main.py --ckpt pretrained_models/afhqdog.pt --PCA_path pretrained_models/PCA_afhqdog.npz --size 512
